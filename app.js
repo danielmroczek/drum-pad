0f104b63c4ef97f14f3455bdf7dbae484e6a0a6d
Daniel Mroczek <dmroczek@gmail.com>
2022-04-22 12:10:51 +0200

    keyboard hooks

--- app.js
+++ app.js
@@ -5,26 +5,58 @@ window.addEventListener('load', () => {
 class DrumPad {
     constructor() {
         this.drumset = 'lm1';
+        this.pads = [...document.querySelectorAll('section.pads .pad')];
+        this.initializeSamples();
         this.addPadHooks();
+        this.addKeyboardHooks();
+    }
+
+    initializeSamples() {
+        this.pads.forEach(pad => {
+            pad.howl = new Howl({
+                src: [`samples/${this.drumset}/${pad.dataset.sound}.wav`]
+            });
+        })
     }
 
     addPadHooks() {
-        document.querySelectorAll('section.pads .pad').forEach(padEl => {
-            let howl = new Howl({
-                src: [`samples/${this.drumset}/${padEl.dataset.sound}.wav`]
-            });
-
-            const playSample = e => {
-                howl.stop();
-                howl.play();
-            };
-
+        this.pads.forEach(pad => {
             if ('ontouchstart' in document.documentElement) {
-                padEl.addEventListener('touchstart', playSample);
+                pad.addEventListener('touchstart', this.playSample);
             } else {
-                padEl.addEventListener('mousedown', playSample);
+                pad.addEventListener('mousedown', this.playSample);
             }
             
+        });
+    }
+
+    playSample({target: pad}) {
+        pad.howl.stop();
+        pad.howl.play();
+    }
+
+    addKeyboardHooks() {
+        const keyToPadMap = {};
+        this.pads.forEach(pad => {
+            const {dataset: {key}} = pad;
+            if (!key) return;
+            keyToPadMap[key.toLowerCase()] = pad;
+        });
+
+        window.addEventListener('keydown', e => {
+            if (e.repeat) return;
+            const pad = keyToPadMap[e.key.toLowerCase()];
+            if (!pad) return;
+
+            pad.dispatchEvent(new Event('mousedown'));
+            pad.classList.add('active')
+        });
+
+        window.addEventListener('keyup', e => {
+            const pad = keyToPadMap[e.key.toLowerCase()];
+            if (!pad) return;
+
+            pad.classList.remove('active')
         });
     }
 }
